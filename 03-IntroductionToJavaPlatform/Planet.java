ab54f5be575db7288e66acbc00bf9d06bdcd1420
theojava <68138181+theojava@users.noreply.github.com>
2020-07-12 11:29:45 -0400

    Update Planet.java
    remove theo

--- 03-IntroductionToJavaPlatform/Planet.java
+++ 03-IntroductionToJavaPlatform/Planet.java
@@ -1,7 +1,7 @@
 class Planet {
 
     void revolve() {
-         System.out.println("Revolve by theo");
+         System.out.println("Revolve");
     }
 
     public static void main(String[] args) {
